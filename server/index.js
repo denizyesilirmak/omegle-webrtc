629296701eefc1914325672c628017a9036ba047
denizyesilirmak <denizyesilirmak@gmail.com>
2019-10-09 01:48:35 +0300

    ADDED: Client Home Compoenent

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const ws = require('ws')
+const http = require('http')
+const express = require('express')
+const app = express()
 
+const server = http.createServer(app)
+const SERVER_PORT = 8080
 const socketServer = new ws.Server({ server })
 
 socketServer.on('connection', (client) => {
     console.log("connection")
+    client.send("nan")
+})
+
+server.listen(SERVER_PORT, () => {
+    console.log(`Server started at ${SERVER_PORT}...`)
 })
